00195e77d6bf72c9041b7ee8e233d4d65054825e
brancardabet <145487305+brancardabet@users.noreply.github.com>
2023-09-19 17:07:41 +0000

    up

---
+++ c-o-m-i-c-d-a-y-s.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         c-o-m-i-c-d-a-y-s
+// @namespace    https://github.com/brancardabet/moi_thuong_tam/raw/main/c-o-m-i-c-d-a-y-s.user.js
+// @version      2023.09.19
+// @description  down c-o-m-i-c-d-a-y-s
+// @author       You
+// @match        https://comic-days.com/episode/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=comic-days.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/brancardabet/moi_thuong_tam/raw/main/c-o-m-i-c-d-a-y-s.user.js
+// @updateURL    https://github.com/brancardabet/moi_thuong_tam/raw/main/c-o-m-i-c-d-a-y-s.meta.js
+// ==/UserScript==
